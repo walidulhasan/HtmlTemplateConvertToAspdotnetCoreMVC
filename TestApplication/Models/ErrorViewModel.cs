b91338ebe5ba9d58de5993f9da15e4befb7064ab
walidulhasan <walidulhasanboniamin@gmail.com>
2022-12-16 11:15:18 +0600

    Add project files.

---
+++ TestApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
